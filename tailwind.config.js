9b5b63e96c5443ee83482218096377305921bff8
allabouttech0803 <allabouttech0803@gmail.com>
2021-05-14 12:00:28 +0530

    Upgrade to Angular 12

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
 module.exports = {
-  prefix: '',
   purge: {
+    enabled: true,
+    content: ['./src/**/*.{html,ts}']
   },
-  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [require('@tailwindcss/forms'),require('@tailwindcss/typography')],
+  variants: {},
+  plugins: [],
 };
